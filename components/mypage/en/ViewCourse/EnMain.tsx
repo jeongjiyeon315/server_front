5422c977c02f5cc07395d81c4b48e82e52027264
jeongjiyeon315 <jung_j_yeon@naver.com>
2021-11-01 12:57:02 +0900

    수정

--- components/mypage/en/ViewCourse/EnMain.tsx
+++ components/mypage/en/ViewCourse/EnMain.tsx
@@ -13,7 +13,7 @@ const EnViewMain = () => {
   const [exist, setExsist] = useState(false);
   useEffect(() => {
     axios
-      .get(`/api/en/course/findall/${memberid}`)
+      .get(process.env.REACT_DB_HOST + `/api/en/course/findall/${memberid}`)
       .then(async (response) => {
         if (response.data.data.length !== 0) {
           setExsist(true);
