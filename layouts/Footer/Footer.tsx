eaebabd528766e8f0eb1379acbb95a32d539f6e7
jeongjiyeon315 <jung_j_yeon@naver.com>
2021-11-03 09:05:54 +0900

    Footer 추가

--- layouts/Footer/Footer.tsx
+++ layouts/Footer/Footer.tsx
@@ -4,26 +4,32 @@ import './Style.scss';
 const Footer = () => {
   return (
     <footer className="Footer">
-      <div className="footer_contents" style={{fontSize:'12px', color:'rgb(186 186 186)', paddingLeft:'10vw'}}>
-        <span className="Title" style={{padding:'20px 0px 10px 0px', display:'inline-block', margin:'0', color:'rgb(92 88 88)'}}>U SPOT</span>
-        <div style={{fontSize:'13px'}}>
-          © 2021 GANGGANG All rights reserved.
-        </div>
-        <div style={{fontSize:'12px'}}>
-          <br/>
+      <div className="footer_contents" style={{ fontSize: '12px', color: 'rgb(186 186 186)', paddingLeft: '10vw' }}>
+        <span
+          className="Title"
+          style={{ padding: '20px 0px 10px 0px', display: 'inline-block', margin: '0', color: 'rgb(92 88 88)' }}
+        >
+          U SPOT
+        </span>
+        <div style={{ fontSize: '13px' }}>© 2021 GANGGANG All rights reserved.</div>
+        <div style={{ fontSize: '12px' }}>
+          <br />
           문예완 mool1997@naver.com
-          <br/>
+          <br />
           이예진 yejin9487@daum.net
-          <br/>
+          <br />
           정지연 jung_j_yeon@naver.com
-          <br/>
+          <br />
           노하늘 gksmf001105@naver.com
-          <br/>
+          <br />
           윤주리 dbswnfl2@naver.com
-          <br/>
+          <br />
+          <p className="umm" style={{ paddingTop: '0', color: 'rgb(141 141 141' }}>
+            추가적인 모델 학습 중이므로, 도시는 계속 업데이트 예정입니다.
+          </p>
         </div>
-        <br/>
-        <br/>
+        <br />
+        <br />
       </div>
     </footer>
   );
